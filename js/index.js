9362e143abdf23def8188e57b69b094e09fe2927
subhrajit-dey <deysubharajit@gmail.com>
2022-12-11 16:20:38 +0530

    Fixed a Few Bugs

--- js/index.js
+++ js/index.js
@@ -4,12 +4,14 @@ let show_more_text = document.querySelector('.show_more_text');
 let show_less_button = document.querySelector('.show_less_button');
 
 show_more_button.addEventListener('click', ()=>{
+    show_more_button.style.display = 'none';
     points.style.display = 'none';
     show_more_text.style.display = 'block';
 })
 
 
 show_less_button.addEventListener('click', ()=>{
+    show_more_button.style.display = 'block';
     points.style.display = 'inline';
     show_more_text.style.display = 'none';
 })
