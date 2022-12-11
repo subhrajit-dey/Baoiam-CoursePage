8be104844373858778d07367d1ee690e2c40aee2
subhrajit-dey <deysubharajit@gmail.com>
2022-12-11 15:56:30 +0530

    Created Main Card

---
+++ js/index.js
@@ -0,0 +1,16 @@
+let show_more_button = document.querySelector('.show_more_button');
+let points = document.querySelector('.points');
+let show_more_text = document.querySelector('.show_more_text');
+let show_less_button = document.querySelector('.show_less_button');
+
+show_more_button.addEventListener('click', ()=>{
+    points.style.display = 'none';
+    show_more_text.style.display = 'block';
+})
+
+
+show_less_button.addEventListener('click', ()=>{
+    points.style.display = 'inline';
+    show_more_text.style.display = 'none';
+})
+
