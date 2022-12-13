525e0e6709d058adae348e38712a882e5e2e0bf8
subhrajit-dey <deysubharajit@gmail.com>
2022-12-14 02:14:43 +0530

    Added the Sticky Header

--- js/index.js
+++ js/index.js
@@ -18,3 +18,30 @@ show_less_button.addEventListener('click', ()=>{
     show_more_text.style.display = 'none';
     selectionClass.classList.toggle('blur');
 })
+
+
+//Header Section on Scroll
+
+let header_selector = document.querySelector('.header_selector');
+let heading_selector = document.querySelector('.heading_selector');
+let tagline_selector = document.querySelector('.tagline_selector');
+let rating_selector = document.querySelector('.rating_selector');
+let created_by_selector = document.querySelector('.created_by_selector');
+let description_selector = document.querySelector('.description_selector');
+
+console.log(header_selector);
+
+window.addEventListener('scroll', ()=>{
+    header_selector.classList.toggle("sticky", window.scrollY>10);
+    // header_selector.classList.toggle("header_section", window.scrollY>10);
+    heading_selector.classList.toggle("sticky_heading", window.scrollY>10);
+    // heading_selector.classList.toggle("heading", window.scrollY>10);
+    tagline_selector.classList.toggle("sticky_tagline", window.scrollY>10);
+    // tagline_selector.classList.toggle("tagline", window.scrollY>10);
+    rating_selector.classList.toggle("sticky_rating", window.scrollY>10);
+    // rating_selector.classList.toggle("rating", window.scrollY>10);
+    created_by_selector.classList.toggle("sticky_created_by", window.scrollY>10);
+    // created_by_selector.classList.toggle("created_by", window.scrollY>10);
+    description_selector.classList.toggle("sticky_description", window.scrollY>10);
+    // description_selector.classList.toggle("description", window.scrollY>10);
+})
